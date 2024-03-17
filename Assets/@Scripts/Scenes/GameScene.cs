252eebcdb9540184d746ca329eb421d946107f09
dovewith999 <51079154+dovewith999@users.noreply.github.com>
2024-03-17 18:17:56 +0900

    NEW : 데이터 세분화 및 리팩토링까지 완료

--- Assets/@Scripts/Scenes/GameScene.cs
+++ Assets/@Scripts/Scenes/GameScene.cs
@@ -23,7 +23,10 @@ public class GameScene : BaseScene
 
         for (int i = 0; i < 5; ++i)
         {
-            Hero hero = Managers.Object.Spawn<Hero>(new Vector3Int(-10 + Random.Range(-5, 5), -5 + Random.Range(-5, 5), 0), HERO_KNIGHT_ID);
+            int heroTemplateID = HERO_WIZARD_ID + Random.Range(0, 5);
+            //int heroTemplateID = HERO_KNIGHT_ID;
+            //int heroTemplateID = HERO_WIZARD_ID;
+            Hero hero = Managers.Object.Spawn<Hero>(new Vector3Int(-10 + Random.Range(-5, 5), -5 + Random.Range(-5, 5), 0), heroTemplateID);
         }
 
         CameraController camera = Camera.main.GetOrAddComponent<CameraController>();
@@ -33,13 +36,14 @@ public class GameScene : BaseScene
 
         {
             Managers.Object.Spawn<Monster>(new Vector3Int(0, 1, 0), MONSTER_BEAR_ID);
+            Managers.Object.Spawn<Monster>(new Vector3Int(1, 1, 0), MONSTER_SLIME_ID);
+            Managers.Object.Spawn<Monster>(new Vector3Int(1, 1, 0), MONSTER_GOBLIN_ARCHER_ID);
 
-            //Managers.Object.Spawn<Monster>(new Vector3Int(1, 1, 0), MONSTER_SLIME_ID);
+        }
 
+        {
             Env env = Managers.Object.Spawn<Env>(new Vector3(0, 2, 0), ENV_TREE1_ID);
             env.EnvState = EEnvState.Idle;
-   
-
         }
 
         return true;
